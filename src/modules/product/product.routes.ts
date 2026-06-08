e4b7e7a8e885c4a178ae3f9c288d029d8cb7923b
saad-dev254 <74713514+saad-dev254@users.noreply.github.com>
2026-06-08 18:19:52 +0500

    code updated

--- src/modules/product/product.routes.ts
+++ src/modules/product/product.routes.ts
@@ -9,7 +9,7 @@ export const productRouter = Router();
 // protected
 productRouter.post("/add-product", requireAuth, productImageUpload.single("productImage"), createProduct);
 productRouter.put("/update-product", requireAuth, productImageUpload.single("productImage"), updateProduct);
-productRouter.get("/all-products", requireAuth, getAllProducts);
+productRouter.post("/all-products", requireAuth, getAllProducts);
 productRouter.post("/get-product-detail", requireAuth, getSingleProduct);
 productRouter.delete("/delete-product", requireAuth, deleteProduct);
 productRouter.get("/get-near-by-products", requireAuth, getNearbyBakeryProducts);
